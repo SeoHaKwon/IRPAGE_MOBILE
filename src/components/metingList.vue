11b4f12935939ee67564d2cee076d46438a8624d
chan <kimynam1@naver.com>
2020-01-18 20:28:54 +0900

    2차 작업진행

---
+++ src/components/metingList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="wrap">
+    <div class="header">
+      <a href="#" class="btn-link"></a>
+      <strong>IR 미팅 내역</strong>
+    </div>
+    <div class="content">
+
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'AlarmList'
+}
+</script>
+ <style lang="scss" scoped>
+ .wrap {
+   display:flex;
+   flex-direction: column;
+   height:100%;
+   .content {
+     padding:0 34px;
+   padding-bottom:74px;
+   }
+ }
+ .header {
+   position: relative;;
+   padding-top:14px;
+   margin-bottom:56px;
+   text-align: center;
+   .btn-link {
+     display:inline-block;
+     position:absolute;
+     left:34px;
+     top:22px;
+     width:10px;
+     height:10px;
+     transform: rotate(45deg);
+     border:2px solid #000;
+     border-top:none;
+     border-right:none;
+     -webkit-tap-highlight-color:transparent;
+   }
+   strong {
+      font-weight: 500;
+      font-size: 18px;
+      line-height: 26px;
+     }
+}
+ </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,15 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <metingList/>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import metingList from '@/components/metingList.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    metingList
   }
 }
 </script>
